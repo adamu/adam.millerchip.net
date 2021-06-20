709623d65f18f6eb0b8d93a685abfca74cde0bb2
Adam Millerchip <adam@millerchip.net>
2021-06-21 01:18:28 +0900

    Build to orphan docs branch for Github Pages

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 tiddlywiki --output docs --build static
+git branch -D docs
+git checkout --orphan docs
+git reset
+git add docs
+git commit --message "Generate static site"
+git clean -f .
+git checkout main
+echo "git push --force origin docs"
